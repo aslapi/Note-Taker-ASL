daf10d9cbffd870336ca6bed6bf79297a8a84d42
Alex LaPierre <aslapierre7@gmail.com>
2024-05-23 17:59:13 -0500

    added get route for main page, get route for notes page, get route for api page, post route for adding notes to json file, delete route for deleting tasks based on id

--- server.js
+++ server.js
@@ -40,7 +40,6 @@
 //   console.log(`App is listening on Port: ${PORT}`);
 // });
 
-
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
@@ -51,27 +50,67 @@ const app = express();
 //Middleware
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //ROOT GET ROUTE HOMEPAGE
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "index.html");
 });
 
 //GET ROUTE FOR USERS TO SEE NOTES PAGE
-app.get('/notes', (req, res) => {
+app.get("/notes", (req, res) => {
   res.sendFile(__dirname + "/public/notes.html");
 });
 
 //GET ROUTE FOR JSON NOTES
-app.get('/api/notes', (req, res) => {
-  fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
-    if (err) {
-      console.log(err)
-    }
-    else {
+app.get("/api/notes", (req, res) => {
+  fs.readFile(__dirname + "/db/db.json", "utf8", (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
       let notes = JSON.parse(data);
       res.json(notes);
+    }
+  });
+});
+
+//POST ROUTE TO READ JSON FILE AND ADD NEW NOTE
+app.post("/api/notes", (req, res) => {
+  fs.readFile(__dirname + "/db/db.json", "utf8", (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      let notes = JSON.parse(data);
+      let newNote = req.body;
+      newNote.id = notes.length + 1;
+      notes.push(newNote);
+      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.json(newNote);
+        }
+      });
+    }
+  });
+});
+
+//DELETE ROUTE USING UNIQUE ID
+app.delete("/api/notes/:id", (req, res) => {
+  let id = req.params.id;
+  fs.readFile(__dirname + "/db/db.json", "utf8", (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      let notes = JSON.parse(data);
+      notes = notes.filter(note => note.id != id);
+      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.json({ ok : true})
+        }
+      })
     }
   })
 })
@@ -80,4 +119,3 @@ app.get('/api/notes', (req, res) => {
 app.listen(PORT, () => {
   console.log("Application listening on " + PORT);
 });
-
